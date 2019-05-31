5d4f6a0f76a8c56f5505275a21643026dd688329
huangdijia <huangdijia@gmail.com>
2019-05-31 10:38:02 +0800

    require laravel/tinker

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,6 +79,7 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Laravel\Tinker\TinkerServiceProvider::class);
 $app->register(Huangdijia\Trigger\TriggerServiceProvider::class);
 
 /*
