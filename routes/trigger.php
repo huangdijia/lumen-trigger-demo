5849ab7bd7f9583cf3280e51f6b388d78412f92a
huangdijia <huangdijia@gmail.com>
2019-05-31 10:27:01 +0800

    first commit

---
+++ routes/trigger.php
@@ -0,0 +1,11 @@
+<?php
+
+use Huangdijia\Trigger\Facades\Trigger;
+
+// Trigger::on('database.table', 'write', function ($event) { dump($evnet); });
+// Trigger::on('database.table', 'update', function ($event) { dump($evnet); });
+// Trigger::on('database.table', 'delete', function ($event) { dump($evnet); });
+
+Trigger::on('*', 'heartbeat', function($event) {
+    dump($event->__toString());
+});
