0359c20e16385141e6b2daef90eb4d51da0dd777
huangdijia <huangdijia@gmail.com>
2019-09-18 13:11:59 +0800

    update laravel-trigger to 2.0

--- routes/trigger.php
+++ routes/trigger.php
@@ -1,26 +1,5 @@
 <?php
 
-use Huangdijia\Trigger\Facades\Trigger;
-
-// Trigger::on('database.table', 'write', function ($event) { dump($evnet); });
-// Trigger::on('database.table', 'update', function ($event) { dump($evnet); });
-// Trigger::on('database.table', 'delete', function ($event) { dump($evnet); });
-
-class Foo
-{
-    public static function bar($event) {
-        dump($event->__toString());
-    }
-}
-
-// action as callable
-Trigger::on('*', 'heartbeat', 'Foo@bar');
-Trigger::on('*', 'heartbeat', ['Foo', 'bar']);
-
-// action as Closure
-Trigger::on('*', 'heartbeat', function($event) {
-    dump($event->__toString());
+$trigger->on('*', 'heartbeat', function($event) use ($trigger) {
+    $trigger->heartbeat($event);
 });
-
-// action as job
-Trigger::on('*', 'heartbeat', 'App\Jobs\ExampleJob');
